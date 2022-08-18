f489f16d1338206b66e67302fa72824930f39e55
Gustav0Furtad0 <gufurtado02@gmail.com>
2022-08-18 10:35:42 -0300

    correcao json para obj

---
+++ js/algomons.js
@@ -0,0 +1,20 @@
+const algomons = [
+    {
+        nome: "Bartossauro",
+        atk: 5,
+        HP: 20,
+        type: "C"
+    },
+    {
+        nome: "Leonildron",
+        atk: 3,
+        HP: 40,
+        type: "R"
+    },
+    {
+        nome: "Saladinei",
+        atk: 4,
+        HP: 30,
+        type: "D"
+    }
+];
