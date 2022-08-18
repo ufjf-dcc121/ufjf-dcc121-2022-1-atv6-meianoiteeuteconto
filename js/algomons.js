0d4de42381815095febfaebdabccd9501d4b9bf5
Pedro Detoni Pereira <pdpereira2003@gmail.com>
2022-08-18 11:24:00 -0300

    criacao de algomons

--- js/algomons.js
+++ js/algomons.js
@@ -3,7 +3,7 @@ const algomons = [
         nome: "Bartossauro",
         atk: 5,
         HP: 20,
-        type: "C"
+        type: "C",
     },
     {
         nome: "Leonildron",
@@ -16,5 +16,78 @@ const algomons = [
         atk: 4,
         HP: 30,
         type: "D"
-    }
+    },
+    {
+        nome: "Gustavasco",
+        atk: 2,
+        HP: 50,
+        type: "D"
+    },
+    {
+        nome: "Nabox",
+        atk: 3,
+        HP: 30,
+        type: "C"
+    },
+    {
+        nome: "Djotade",
+        atk: 4,
+        HP: 20,
+        type: "R"
+    },
+    {
+        nome: "Bushoba",
+        atk: 2,
+        HP: 40,
+        type: "D"
+    },
+    {
+        nome: "Leneka",
+        atk: 3,
+        HP: 40,
+        type: "C"
+    },
+    {
+        nome: "Savonimo",
+        atk: 3,
+        HP: 40,
+        type: "C"
+    },
+    {
+        nome: "Vinissudo",
+        atk: 4,
+        HP: 20,
+        type: "D"
+    },
+    {
+        nome: "Jacomelson",
+        atk: 5,
+        HP: 20,
+        type: "R"
+    },
+    {
+        nome: "Leanel",
+        atk: 5,
+        HP: 40,
+        type: "R"
+    },
+    {
+        nome: "Hugordola",
+        atk: 3,
+        HP: 40,
+        type: "C"
+    },
+    {
+        nome: "Zelunsio",
+        atk: 5,
+        HP: 50,
+        type: "D"
+    },
+    {
+        nome: "Pepeco",
+        atk: 1,
+        HP: 10,
+        type: "C"
+    },
+    
 ];
